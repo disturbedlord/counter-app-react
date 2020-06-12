64e321ee0c82294c3ab46b4117f35775c3f54a35
avi <avi@kali.avi.com>
2020-06-13 00:29:28 +0530

    pushing Files

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,45 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
+    tags: [],
   };
+
+  handleIncrement = () => {
+    this.state.count++;
+    this.setState({});
+  };
+
   render() {
+    this.getCounterColor();
+
     return (
-      <React.Fragment>
-        <span>{this.formatCounter()}</span>
-        <button>Button</button>
-      </React.Fragment>
+      <div>
+        <span className={this.getCounterColor()}>{this.formatCounter()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.length > 0 ? (
+            this.state.tags.map((tag) => <li key={tag}>{tag}</li>)
+          ) : (
+            <p>No Data</p>
+          )}
+        </ul>
+      </div>
     );
   }
 
-  formatCounter = () =>
-    this.state.count === 0 ? <h1>Zero</h1> : this.state.count;
+  formatCounter = () => (this.state.count === 0 ? "Zero" : this.state.count);
+
+  getCounterColor() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
 }
 
 export default Counter;
